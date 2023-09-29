be4ff4692154476b232809413871bbc3d084ba2f
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-29 14:05:59 +0200

    export logic to ./csv-parse.js file

--- main.js
+++ main.js
@@ -1,80 +1,9 @@
 import fs from 'node:fs';
-import stream from 'node:stream';
-
-/**
- * @param {string} line
- * @param {string} separator
- * @param {string} quote
- * @returns {string[]}
- */
-function csvSplitLine(line, separator, quote) {
-  const values = [];
-
-  let value = '';
-  let isInsideQuotes = false;
-
-  for (const char of line) {
-    if (char === separator && !isInsideQuotes) {
-      values.push(value);
-
-      value = '';
-    } else if (char === quote) {
-      isInsideQuotes = !isInsideQuotes;
-    } else {
-      value += char;
-    }
-  }
-
-  values.push(value);
-
-  return values;
-}
-
-/**
- * @param {stream.TransformOptions} options
- * @returns {stream.Transform}
- */
-function createCsvParseTransformStream(options) {
-  return new stream.Transform({
-    ...options,
-
-    readableObjectMode: true,
-
-    construct(callback) {
-      this.chunkRest = '';
-      this.separator = ',';
-      this.quote = '"';
-
-      callback();
-    },
-
-    transform(chunk, encoding, callback) {
-      const lines = (this.chunkRest + chunk.toString()).split('\n');
-
-      this.chunkRest = lines.pop();
-
-      for (const line of lines) {
-        const values = csvSplitLine(line, this.separator, this.quote);
-
-        this.push(values);
-      }
-
-      callback();
-    },
-
-    flush(callback) {
-      if (this.chunkRest) {
-        this.push(this.chunkRest);
-      }
-
-      callback();
-    },
-  });
-}
+import csvParse from './csv-parse.js';
 
 const readStream = fs.createReadStream('./5-users.csv', { encoding: 'utf-8' });
 
-const csvParseTransformStream = createCsvParseTransformStream();
+const csvParseTransformStream = csvParse.createTransformStream();
 
 readStream.pipe(csvParseTransformStream);
 
