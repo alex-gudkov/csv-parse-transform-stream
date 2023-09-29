1ecaa2fd75c13e613a97f622e819ede04461e223
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-29 11:27:43 +0200

    just read file

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('csv-parse-transform-stream');
+import fs from 'node:fs';
+
+fs.readFile('./5-users.csv', 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+
+    return;
+  }
+
+  console.log(data);
+});
