607c63f4cbf74c2a34744c4abf0a33be89188246
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-29 15:52:37 +0200

    add some changes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import fs from 'node:fs';
-import { CsvParseTransformStream } from './csv/csv-parse-transform-stream.js';
+import { CsvParseTransformStream } from './csv-parse-transform-stream.js';
 
-const readStream = fs.createReadStream('./data/5-users.csv', { encoding: 'utf-8' });
+const filePath = './data/5-users.csv';
+const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
 
 const csvParseTransformStream = new CsvParseTransformStream();
 
-readStream.pipe(csvParseTransformStream);
-
-csvParseTransformStream.on('data', (chunk) => {
-  console.log({ chunk });
-});
+readStream
+  .pipe(csvParseTransformStream)
+  .on('data', (chunk) => {
+    console.log({ chunk });
+  });
